1bc77f25189eb48739495da59472f8b69b2ae96d
LaureB <lbleyer@outlook.fr>
2026-02-26 09:34:16 +0100

    wip

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import * as React from "react";
 import { Box } from "../assets/mui";
-import IconMenu from "../components/Menu.jsx";
+import Menu from "../components/Menu.jsx";
 import FeedPublic from "../components/FeedPublic.jsx";
 
 export default function Home({ user }) {
   return (
     <Box sx={{ display: "flex", gap: 2, p: 2 }}>
-      <IconMenu />
+      <Menu />
       <Box sx={{ flex: 1 }}>
         <FeedPublic user={user} />
       </Box>
